8810e40ec58565cdc5693bcbbf903e474fbe5f92
ryanstapleton <megalixir.1@gmail.com>
2018-02-22 11:26:38 -0700

    Integrated growl notifications for active record errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,7 @@ class PostsController < ApplicationController
 
   def update
     if @post.update(post_params)
-      redirect_to @post, notice: "Your post was created updated."
+      redirect_to @post, notice: "Your post was successfully updated."
     else
       render :edit
     end
