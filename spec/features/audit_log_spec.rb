7dd75d2289babe0f6453c8ebf9995697f66b0fc6
ryanstapleton <megalixir.1@gmail.com>
2018-03-06 16:31:52 -0700

    Blocked non admin users from accessing index page for audits

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -17,6 +17,13 @@ describe 'AuditLog Feature' do
       expect(page).to have_content(/SNOW/)
     end
 
-    xit 'cannot be accessed by non admin users'
+    it 'cannot be accessed by non admin users' do
+      logout(:user)
+      user = FactoryBot.create(:user)
+      login_as(user, :scope => :user)
+
+      visit audit_logs_path
+      expect(current_path).to eq(root_path)
+    end
   end
 end
