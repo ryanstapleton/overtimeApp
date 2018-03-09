6a25f4037288ad51ea4bf0f64f69d786a6d6c2a0
ryanstapleton <megalixir.1@gmail.com>
2018-03-09 13:26:27 -0700

    Added production url to action mailer default

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.serve_static_assets = true
+
+  config.action_mailer.default_url_options = { :host => 'overtime-devcamp.herokuapp.com/' }
 end
