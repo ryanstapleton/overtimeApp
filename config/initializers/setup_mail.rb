fd2eb90dc20167f7057cdfb231594e62672d0276
ryanstapleton <megalixir.1@gmail.com>
2018-03-09 13:54:27 -0700

    Updated mail server settings

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,8 +1,8 @@
 ActionMailer::Base.smtp_settings = {
-    :port =>           ENV['SPARKPOST_SMTP_PORT'],
-    :address =>        ENV['SPARKPOST_SMTP_HOST'],
-    :user_name =>      ENV['SPARKPOST_SMTP_USERNAME'],
-    :password =>       ENV['SPARKPOST_SMTP_PASSWORD'],
+    :port =>           ENV['MAILGUN_SMTP_PORT'],
+    :address =>        ENV['MAILGUN_SMTP_SERVER'],
+    :user_name =>      ENV['MAILGUN_SMTP_LOGIN'],
+    :password =>       ENV['MAILGUN_SMTP_PASSWORD'],
     :authentication => :login,
     :enable_starttls_auto => true
 }
