000bfc3d5597595c1e863036772e40ff3c5f51b9
ryanstapleton <megalixir.1@gmail.com>
2018-03-15 11:48:14 -0600

    Changed ping file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var http = require("http");
+setInterval(function() {
+    http.get("http://overtime-devcamp.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
